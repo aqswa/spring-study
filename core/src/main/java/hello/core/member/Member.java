c582a478d8d01874ac3eb98d1dc135b02f145a50
aqswa <joayoung3@gmail.com>
2024-01-24 21:28:46 +0900

    다양한 설정 형식 지원 - 자바 코드, XML

--- core/src/main/java/hello/core/member/Member.java
+++ core/src/main/java/hello/core/member/Member.java
@@ -15,9 +15,7 @@ public class Member {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    public void setId(Long id) { this.id = id; }
 
     public String getName() {
         return name;
